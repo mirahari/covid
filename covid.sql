a43395701e2ed0f728eb8b0e2e22d821326c46fb
mirahari <91404276+mirahari@users.noreply.github.com>
2021-09-27 18:27:57 +0800

    Update covid.sql

--- covid.sql
+++ covid.sql
@@ -1,14 +1,14 @@
 -- SELECT DATA
 
 SELECT location, date, total_cases, new_cases, total_deaths, population
-FROM Covid19..CovidDeaths$
+FROM Covid19..CovidDeaths
 ORDER BY 1,2
 
 
 -- Total cases VS Total deaths (Likelihood of death)
 
 SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as death_rate
-FROM Covid19..CovidDeaths$
+FROM Covid19..CovidDeaths
 WHERE location like '%states%'
 ORDER BY 1,2
 
@@ -16,7 +16,7 @@ ORDER BY 1,2
 -- Total cases vs Population
 
 SELECT location, date, population, total_cases, (total_cases/population)*100 as infection_rate
-FROM Covid19..CovidDeaths$
+FROM Covid19..CovidDeaths
 WHERE location like '%states%'
 ORDER BY 1,2
 
@@ -25,7 +25,7 @@ ORDER BY 1,2
 
 SELECT location, population, MAX(total_cases) as highest_infection_count, MAX(total_cases/population)*100 as 
 	max_infection_rate
-FROM Covid19..CovidDeaths$
+FROM Covid19..CovidDeaths
 GROUP BY location, population
 ORDER BY max_infection_pct DESC
 
@@ -33,7 +33,7 @@ ORDER BY max_infection_pct DESC
 -- Countries with Highest Death Count per Population
 
 SELECT location, MAX(cast(total_deaths AS INT)) as highest_death_count
-FROM Covid19..CovidDeaths$
+FROM Covid19..CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY location
 ORDER BY highest_death_count DESC
@@ -42,13 +42,13 @@ ORDER BY highest_death_count DESC
 -- Continent with Highest Death Count
 
 --SELECT continent, MAX(cast(total_deaths AS INT)) as highest_death_count
---FROM Covid19..CovidDeaths$
+--FROM Covid19..CovidDeaths
 --WHERE continent IS NOT NULL
 --GROUP BY continent
 --ORDER BY highest_death_count DESC
 
 SELECT location, MAX(cast(total_deaths AS INT)) as highest_death_count
-FROM Covid19..CovidDeaths$
+FROM Covid19..CovidDeaths
 WHERE continent IS NULL
 GROUP BY location
 ORDER BY highest_death_count DESC
@@ -58,7 +58,7 @@ ORDER BY highest_death_count DESC
 
 SELECT date, SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths,
 	SUM(cast(new_deaths as int))/SUM(new_cases)*100 as death_rate
-FROM Covid19..CovidDeaths$
+FROM Covid19..CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY date
 ORDER BY 1,2
@@ -68,7 +68,7 @@ ORDER BY 1,2
 
 SELECT SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths,
 	SUM(cast(new_deaths as int))/SUM(new_cases)*100 as death_rate
-FROM Covid19..CovidDeaths$
+FROM Covid19..CovidDeaths
 WHERE continent IS NOT NULL
 ORDER BY 1,2
 
@@ -76,13 +76,13 @@ ORDER BY 1,2
 -- Total Population vs Vaccinations
 
 SELECT dth.continent, dth.location, dth.date, dth.population, vax.new_vaccinations,	
-	SUM(CONVERT(BIGINT, vax.new_vaccinations)) OVER (PARTITION BY dth.location ORDER BY dth.location, dth.date)
-	AS rolling_vaxxed
-FROM Covid19..CovidDeaths$ dth
-JOIN Covid19..CovidVax$ vax
+SUM(CONVERT(BIGINT, vax.new_vaccinations)) OVER (PARTITION BY dth.location ORDER BY dth.location, dth.date)
+AS rolling_vaxxed
+FROM Covid19..CovidDeaths dth
+JOIN Covid19..CovidVax vax
 	ON dth.location = vax.location
 	AND dth.date = vax.date
-	WHERE dth.continent IS NOT NULL
+WHERE dth.continent IS NOT NULL
 ORDER BY 2,3
 
 
@@ -93,8 +93,8 @@ WITH POPvsVAX (continent, location, date, population, new_vaccinations, rolling_
 SELECT dth.continent, dth.location, dth.date, dth.population, vax.new_vaccinations,	
 	SUM(cast(vax.new_vaccinations AS BIGINT)) OVER (PARTITION BY dth.location ORDER BY dth.location, dth.date)
 	AS rolling_vaxxed
-FROM Covid19..CovidDeaths$ dth
-JOIN Covid19..CovidVax$ vax
+FROM Covid19..CovidDeaths dth
+JOIN Covid19..CovidVax vax
 	ON dth.location = vax.location
 	AND dth.date = vax.date
 WHERE dth.continent IS NOT NULL
@@ -120,8 +120,8 @@ INSERT INTO #VaxPop
 SELECT dth.continent, dth.location, dth.date, dth.population, vax.new_vaccinations,	
 	SUM(cast(vax.new_vaccinations AS BIGINT)) OVER (PARTITION BY dth.location ORDER BY dth.location, dth.date)
 	AS rolling_vaxxed
-FROM Covid19..CovidDeaths$ dth
-JOIN Covid19..CovidVax$ vax
+FROM Covid19..CovidDeaths dth
+JOIN Covid19..CovidVax vax
 	ON dth.location = vax.location
 	AND dth.date = vax.date
 WHERE dth.continent IS NOT NULL
@@ -137,8 +137,8 @@ CREATE VIEW vaxxed_population AS
 SELECT dth.continent, dth.location, dth.date, dth.population, vax.new_vaccinations,	
 	SUM(cast(vax.new_vaccinations AS BIGINT)) OVER (PARTITION BY dth.location ORDER BY dth.location, dth.date)
 	AS rolling_vaxxed
-FROM Covid19..CovidDeaths$ dth
-JOIN Covid19..CovidVax$ vax
+FROM Covid19..CovidDeaths dth
+JOIN Covid19..CovidVax vax
 	ON dth.location = vax.location
 	AND dth.date = vax.date
 WHERE dth.continent IS NOT NULL
